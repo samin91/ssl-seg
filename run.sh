e298765ab516684dbcace583b3d71e5a87e07d1d
samin91 <saminhamidi@yahoo.com>
2025-09-24 09:11:22 +0200

    updates for new runs

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 DATA_PATH="/Users/samin91/Desktop/Projects/ssl-semseg/Collections/Industrial_Burner_Flames_noAugmentation"
-EPOCHS=1
+EPOCHS=5
 BATCH_SIZE=4
 LEARNING_RATE=1e-3
 NUM_CLASSES=2  # grey scale images - foreground and background classes
-PRETRAIN_TYPE="swav"  #["none", "imagenet", "simclr", "moco", "swav"]
+PRETRAIN_TYPE="imagenet"  #["none", "imagenet", "simclr", "moco", "swav"]
 PRETRAIN_PATH="/Users/samin91/Desktop/Projects/ssl-semseg/Weights"  #"Path to self-supervised VISSL weights (.pth)"
 LOGGER_CSV="training_log.csv"
-LOGGER_TENSOIRBOARD="runs/experiment1"
+LOGGER_TENSOIRBOARD="runs"
 
 python3 main.py \
     --data_root $DATA_PATH \
