97addfe880e01a0a29f4e9fc1d45666c4e63242b
samin91 <saminhamidi@yahoo.com>
2025-11-11 16:53:33 +0100

    testing merge request on gitlab

--- utils/test_feature.py
+++ utils/test_feature.py
@@ -1,2 +1,3 @@
 """
-I am testing the merge and pull request of git."""
+I am testing the merge and pull request of git.
+I am adding this part to see why the merge request does not show in the gitlab"""
